5d59350fb69d7a018971a42cf1fbc73d9221b9fd
johnshelb <jsnd11@gmail.com>
2017-08-26 23:21:31 -0400

    deal me in

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var upperCorner: UILabel!
+    @IBOutlet weak var middleLabel: UILabel!
+    @IBOutlet weak var lowerCorner: UILabel!
+    
+    @IBOutlet weak var spade: UIButton!
+    @IBOutlet weak var diamond: UIButton!
+    @IBOutlet weak var hearts: UIButton!
+    @IBOutlet weak var cardChoice: UIButton!
     // TODO: Create IB outlets
     
     override func viewDidLoad() {
@@ -20,6 +27,35 @@ class ViewController: UIViewController {
     
     // TODO: IB actions and code to update UI
 
-  
+    @IBAction func drawCard(_ sender: UIButton) {
+        switch sender{
+        case spade:
+            print("♠️")
+            upperCorner.text="♠️"
+            lowerCorner.text="♠️"
+            middleLabel.text="3"
+        case cardChoice:
+            print("♣️")
+            upperCorner.text="♣️"
+            lowerCorner.text="♣️"
+            middleLabel.text="4"
 
+        case hearts:
+            print("♥️")
+            upperCorner.text="♥️"
+            lowerCorner.text="♥️"
+            middleLabel.text = "10"
+
+        case diamond:
+            print("♦️")
+            upperCorner.text="♦️"
+            lowerCorner.text="♦️"
+            middleLabel.text = "8"
+
+        default:
+            print("")
+        }
+    
+    }
+    
 }
